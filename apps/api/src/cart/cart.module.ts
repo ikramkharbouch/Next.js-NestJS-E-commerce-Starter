9342290c8b63bb30ff325307033e9c8551215a5e
ikramkharbouch <4573r14@gmail.com>
2025-10-08 19:00:00 +0100

    fix(api): provide ProductsService in CartModule
    - Export ProductsService from ProductsModule and import it in CartModule
    - Fixes "Nest can't resolve dependencies" error

--- apps/api/src/cart/cart.module.ts
+++ apps/api/src/cart/cart.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from './entities/cart.entity';
+import { CartItem } from './entities/cart-item.entity';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Cart, CartItem]),
+    ProductsModule
+  ],
   providers: [CartService],
-  controllers: [CartController]
+  controllers: [CartController],
+  exports: [CartService]
 })
 export class CartModule {}
